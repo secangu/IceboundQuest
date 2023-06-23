5ae89302728e182468019ef479ff5d4f8b99ff3e
KaptoSp <ytrios21@misena.edu.co>
2023-06-22 23:02:15 -0500

    Update Buttons_sc.cs

--- Icebound Quest URP/Assets/_Icebound Cuest/_Scripts/Interfaces/Buttons_sc.cs
+++ Icebound Quest URP/Assets/_Icebound Cuest/_Scripts/Interfaces/Buttons_sc.cs
@@ -3,16 +3,56 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class Buttons_sc : MonoBehaviour
 {
-    [SerializeField]  AudioMixer audioMixer;
+    [SerializeField]  AudioMixer _audioMixer;
     MouseLock_sc mouseLock;
+    [SerializeField] Slider _sliderGeneralValue;
+    [SerializeField] Slider _sliderMusicValue;
+    [SerializeField] Slider _sliderEffectsValue;
 
+    float _generalVolume;
+    float _musicVolume;
+    float _effectsVolume;
 
     void Start()
     {
         mouseLock=FindObjectOfType<MouseLock_sc>();
+        if (PlayerPrefs.HasKey("VolumenGeneral") == false) PlayerPrefs.SetFloat("VolumenGeneral", 1.5f);
+        if (PlayerPrefs.HasKey("VolumenMusica") == false) PlayerPrefs.SetFloat("VolumenMusica", 1.5f);
+        if (PlayerPrefs.HasKey("VolumenEfectos") == false) PlayerPrefs.SetFloat("VolumenEfectos", 5f);
+
+        _generalVolume = PlayerPrefs.GetFloat("VolumenGeneral");
+        _musicVolume = PlayerPrefs.GetFloat("VolumenMusica");
+        _effectsVolume = PlayerPrefs.GetFloat("VolumenEfectos");
+
+        _sliderGeneralValue.value = _generalVolume;
+        _sliderMusicValue.value = _musicVolume;
+        _sliderEffectsValue.value = _effectsVolume;
+
+        CambiarVolumenGeneral(_generalVolume);
+        CambiarVolumenMusica(_musicVolume);
+        CambiarVolumenEfectos(_effectsVolume);
+    }
+    public void CambiarVolumenGeneral(float volumen)
+    {
+        _audioMixer.SetFloat("VolumenGeneral", Mathf.Log10(volumen) * 20);
+
+        PlayerPrefs.SetFloat("VolumenGeneral", volumen);
+    }
+    public void CambiarVolumenMusica(float volumen)
+    {
+        _audioMixer.SetFloat("VolumenMusica", Mathf.Log10(volumen) * 20);
+
+        PlayerPrefs.SetFloat("VolumenMusica", volumen);
+    }
+    public void CambiarVolumenEfectos(float volumen)
+    {
+        _audioMixer.SetFloat("VolumenEfectos", Mathf.Log10(volumen) * 20);
+
+        PlayerPrefs.SetFloat("VolumenEfectos", volumen);
     }
     public void GameScene(string Scene)
     {
@@ -20,11 +60,11 @@ public class Buttons_sc : MonoBehaviour
     }
     public void ChangeVolume(float volume)
     {
-        audioMixer.SetFloat("Volume",volume);
+        _audioMixer.SetFloat("Volume",volume);
     }
     public void ChangeVolumeSFX(float volumeSfx)
     {
-        audioMixer.SetFloat("Effects", volumeSfx);
+        _audioMixer.SetFloat("Effects", volumeSfx);
     }
     public void Select(AudioSource audioSource)
     {
