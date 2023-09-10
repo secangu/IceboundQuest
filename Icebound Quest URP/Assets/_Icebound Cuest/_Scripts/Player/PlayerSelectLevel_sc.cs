a39f2cd633ff8c912ab2c9040c2cfe4efd51cfa0
secangu <carvajalsergio2018@outlook.es>
2023-09-09 22:41:12 -0500

    Nueva Fuente + Canvas casi completo en escena de Fight :3

--- Icebound Quest URP/Assets/_Icebound Cuest/_Scripts/Player/PlayerSelectLevel_sc.cs
+++ Icebound Quest URP/Assets/_Icebound Cuest/_Scripts/Player/PlayerSelectLevel_sc.cs
@@ -7,6 +7,7 @@ public class PlayerSelectLevel_sc : MonoBehaviour
     [SerializeField] float rotation;
     [SerializeField] int scene;
     bool enter;
+    bool cancel;
 
     Animator animator;
     InterfaceController_sc interfaceController;
@@ -17,8 +18,10 @@ public class PlayerSelectLevel_sc : MonoBehaviour
     }
     private void Update()
     {
-        if (enter && Input.GetKeyUp(KeyCode.F))
-        {
+        if (enter && Input.GetKeyUp(KeyCode.F)&&!cancel)
+        {            
+            cancel = true;
+            enter = false;
             transform.rotation = Quaternion.Euler(0, rotation, 0);
             StartCoroutine(Jump());
         }
